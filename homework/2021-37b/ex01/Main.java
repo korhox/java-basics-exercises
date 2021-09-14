b3f9fa655250016b01296d60a5894a52b2f31b0a
korho <juuso@korho.fi>
2021-09-14 13:38:14 +0300

    Continue

---
+++ homework/2021-37b/ex01/Main.java
@@ -0,0 +1,20 @@
+//
+// Juuso Korhonen <juuso.m.korhonen@tuni.fi>
+// Tässä tiedostossa tehtävä 1
+//
+
+import java.io.Console;
+public class Main {
+    public static void main(String[] args) {
+        Console c = System.console();
+
+        System.out.println("And your name is?");
+        String name = c.readLine();
+
+        if (name.toLowerCase().equals("jussi")) {
+            System.out.println("What a stupid name.");
+        } else {
+            System.out.println("You have a beautiful name!");
+        }
+    }
+}
---
+++ homework/2021-37b/ex02/Main.java
@@ -0,0 +1,14 @@
+//
+// Juuso Korhonen <juuso.m.korhonen@tuni.fi>
+// Tässä tiedostossa tehtävä 2
+//
+
+public class Main {
+    public static void main(String[] args) {
+
+        String text = "This is an test string.";
+        System.out.println(text);
+        
+        System.out.println(text.charAt(5));
+    }
+}
