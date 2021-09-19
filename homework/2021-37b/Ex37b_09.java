7f015a8c5979542f73e4076b1cb2378201e5f607
korho <juuso@korho.fi>
2021-09-19 13:42:23 +0300

    continue

--- homework/2021-37b/Ex37b_09.java
+++ homework/2021-37b/Ex37b_09.java
@@ -11,18 +11,24 @@ public class Ex37b_09 {
 
         System.out.println("And your name is?");
         String name = c.readLine();
+        String nameFiltered = "";
         String nameBackwards = "";
 
-        // Loop backwards through number of letters in name
-        for (int i=name.length(); i>0; i--) {
+        // Loop through the name
+        for (int i=0; i<name.length(); i++) {
             // Add characters to new string one by one
-            if(name.charAt(i-1) == '.') {
-                
+            if(name.charAt(i) != ' ' || name.charAt(i) != '.' || name.charAt(i) != ',') {
+                nameFiltered = nameFiltered + name.charAt(i);
             }
-            nameBackwards = nameBackwards + name.charAt(i-1);
         }
 
-        if (name.equalsIgnoreCase(nameBackwards)) {
+        // Loop backwards through number of letters in name
+        for (int i=nameFiltered.length(); i>0; i--) {
+            // Add characters to new string one by one
+            nameBackwards = nameBackwards + nameFiltered.charAt(i-1);
+        }
+
+        if (nameFiltered.equalsIgnoreCase(nameBackwards)) {
             System.out.println("It is palindrome");
         } else {
             System.out.println("It is not palindrome");
