22a293807daae6a465e5bd620d2fb911ce855e78
korho <juuso@korho.fi>
2021-09-27 14:02:00 +0300

    Add 1-3

--- homework/2021-39/Ex39_02.java
+++ homework/2021-39/Ex39_02.java
@@ -1,12 +1,17 @@
 //
 // Juuso Korhonen <juuso.m.korhonen@tuni.fi>
-// This file has exercise 1
+// This file has exercise 2
 //
 
-class Ex39_1 {
+class Ex39_2 {
     public static void main(String [] args) {
+        tulostaTervehdys();
         tulostaNimi();
     }
+    public static void tulostaTervehdys() {
+        System.out.println("Hei,");
+    }
+
     public static void tulostaNimi() {
         System.out.println("Juuso");
     }
---
+++ homework/2021-39/Ex39_03.java
@@ -0,0 +1,13 @@
+//
+// Juuso Korhonen <juuso.m.korhonen@tuni.fi>
+// This file has exercise 2
+//
+
+class Ex39_3 {
+    public static void main(String [] args) {
+        tulostaJokinNimi("Pena");
+    }
+    public static void tulostaJokinNimi(String nimi) {
+        System.out.println(nimi);
+    }
+}
