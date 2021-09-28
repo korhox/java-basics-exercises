641bfe9cf90caef2988c556d17c3e96dba6d581b
korho <juuso@korho.fi>
2021-09-28 23:14:31 +0300

    Continue

--- homework/2021-39/Ex39_09.java
+++ homework/2021-39/Ex39_09.java
@@ -6,26 +6,26 @@
 import java.io.Console;
 
 class Ex39_9 {    
-        public static void main(String [] args) {
-            asetaA();
-            asetaB();
-            laskeYhteen();
-            tulosta();
-        }
-    
-        public static void asetaA() {
-            int a = 5;
-        }
-    
-        public static void asetaB() {
-            int b = 5;
-        }
-    
-        public static void laskeYhteen() {
-            int summa = a + b;
-        }
-    
-        public static void tulosta() {
-            System.out.println(summa);
-        }
+    public static void main(String [] args) {
+        asetaA();
+        asetaB();
+        laskeYhteen();
+        tulosta();
     }
+
+    public static void asetaA() {
+        int a = 5;
+    }
+
+    public static void asetaB() {
+        int b = 5;
+    }
+
+    public static void laskeYhteen() {
+        int summa = a + b;
+    }
+
+    public static void tulosta() {
+        System.out.println(summa);
+    }
+}
