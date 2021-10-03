e0ceefcb21a3532e9cabe4a0052da5ac8869c20a
korho <juuso@korho.fi>
2021-10-03 20:07:25 +0300

    Add ex 10

---
+++ homework/2021-39/Ex39_10.java
@@ -0,0 +1,22 @@
+//
+// Juuso Korhonen <juuso.m.korhonen@tuni.fi>
+// This file has exercise 10
+//
+
+class Ex39_10 {
+    public static void main(String [] args) {
+        System.out.println(replaceChars("abc:abc:", 'a', "terve"));
+    }
+
+    public static String replaceChars(String subject, char search, String replace) {
+        String result = "";
+        for (char v : subject.toCharArray()) {
+            if (v == search) {
+                result += replace;
+            } else {
+                result += v;
+            }
+        }
+        return result;
+    }
+}
