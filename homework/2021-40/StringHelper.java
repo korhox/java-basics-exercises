277ca562ed0fb8f86bc4fdaeb95405ea992295d7
korho <juuso@korho.fi>
2021-10-07 22:11:04 +0300

    Add 5 and start 6

---
+++ homework/2021-40/StringHelper.java
@@ -0,0 +1,37 @@
+//
+// Juuso Korhonen <juuso.m.korhonen@tuni.fi>
+// This file has exercise 5
+//
+
+class StringHelper {
+    public static void main(String [] args) {
+        if (equals("epic", "epic")) {
+            System.out.println("true");
+        } else {
+            System.out.println("false");
+        }
+        if (equals("epic", "sdd")) {
+            System.out.println("true");
+        } else {
+            System.out.println("false");
+        }
+    }
+
+    public static Boolean equals(String a, String b) {
+        Boolean result = true;
+        for (int i = 0; i<a.length() && result; i++) {
+            if (a.charAt(i) != b.charAt(i)) {
+                result = false;
+            }
+        }
+        return result;
+    }
+
+    public static String [] split(String subject) {
+        for (int i = 0; i < subject.length(); i++) {
+            if(subject.charAt(i) == '.') {
+
+            }
+        }
+    }
+}
