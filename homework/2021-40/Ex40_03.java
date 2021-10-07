129a2942fc4f98af202f0455e3506cccb27f169a
korho <juuso@korho.fi>
2021-10-07 21:44:24 +0300

    Add ex3

---
+++ homework/2021-40/Ex40_03.java
@@ -0,0 +1,16 @@
+//
+// Juuso Korhonen <juuso.m.korhonen@tuni.fi>
+// This file has exercise 3
+//
+
+class Ex40_03 {
+    public static void main(String [] args) {
+        System.out.println(devide(1,1));
+        System.out.println(devide(1,2));
+        System.out.println(devide(2,1));
+    }
+
+    public static Float devide(int a, int b) {
+        return (float) a / b;
+    }
+}
