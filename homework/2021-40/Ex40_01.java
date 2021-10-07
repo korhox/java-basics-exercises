d859ad0e39e432b9912bd2e9eeb3a8eb7ee6c0b0
korho <juuso@korho.fi>
2021-10-07 21:16:49 +0300

    Add Ex40_01

---
+++ homework/2021-40/Ex40_01.java
@@ -0,0 +1,14 @@
+//
+// Juuso Korhonen <juuso.m.korhonen@tuni.fi>
+// This file has exercise 1
+//
+
+class Ex40_01 {
+    public static void main(String [] args) {
+        printDate();
+    }
+
+    public static void printDate() {
+        System.out.println("2021-10-07");
+    }
+}
