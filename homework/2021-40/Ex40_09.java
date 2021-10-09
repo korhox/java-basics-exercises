dd949a9dd78c440aa3a6962ba3555de92f7e1922
korho <juuso@korho.fi>
2021-10-09 12:25:14 +0300

    Add 9

--- homework/2021-40/Ex40_09.java
+++ homework/2021-40/Ex40_09.java
@@ -7,25 +7,17 @@ class Ex40_09 {
     public static void main(String [] args) {
         // Tulostaa true
         System.out.println(contains("koirarotu", "raro"));
-
-        // Tulostaa true
-        System.out.println(contains("koirarotu", "rotu"));
-
+        
         // Tulostaa false
         System.out.println(contains("koirarotu", "tampio"));
     }
 
-    public static boolean contains(String subject, String search) {
-        boolean returnTrue = true;
-        for (int i = 0; i < subject.length(); i++) {
-            for (int j = 0; j < search.length(); j++) {
-                if (j + i < subject.length()) {
-                    if (subject.charAt(j + i) != search.charAt(j)) {
-                        returnTrue = false;
-                    }
-                }
+    public static boolean contains(String subject, String search){
+        for (int i = 0; i <= subject.length() - search.length(); i++) {
+            if(subject.substring(i, i + search.length()).equals(search)){
+                return true;
             }
         }
-        return returnTrue;
+        return false;
     }
 }
